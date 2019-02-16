111321d393bf3d25b599f0230a5fb8d75568879d
GabrielScalici <gabrielhc.scalici@gmail.com>
2019-02-16 19:48:29 -0200

    header and search added

--- fastwallpaper/src/App.js
+++ fastwallpaper/src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+//COMPONENTES
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+//IMAGENS
+import logo from './img/logo.png';
 
-      </div>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                        {' Fast Wallpaper'}
+                    </Navbar.Brand>
+                </Navbar>
+
+                <div className="ui huge fluid action input">
+                    <input type="text" placeholder="Digite em inglês o estilo de papel de parede que deseja..." />
+                    <div className="ui button">Buscar</div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
